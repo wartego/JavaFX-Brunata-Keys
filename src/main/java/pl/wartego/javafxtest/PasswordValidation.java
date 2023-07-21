55da0e7aacf9ebf5b151928d224f5608eaede80e
Tomek <wartego@wp.pl>
2023-07-21 10:20:08 +0200

    PasswordValidation add Bcrypt validation.

--- src/main/java/pl/wartego/javafxtest/PasswordValidation.java
+++ src/main/java/pl/wartego/javafxtest/PasswordValidation.java
@@ -4,14 +4,20 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 
 public class PasswordValidation {
 
-    private static String password = "ss";
-    private static String hashedPassword = BCrypt.hashpw(password,BCrypt.gensalt(10));
-
-    public static void checkPass(String password){
+        public static String HashPassword(String password){
+        String hashedPassword = BCrypt.hashpw(password,BCrypt.gensalt(10));
         if(BCrypt.checkpw(password,hashedPassword)){
             System.out.println("Password Match");
+            return hashedPassword;
         } else {
             System.out.println("No match");
+            return "x";
         }
     }
+
+
+    public static boolean ifHashMatchToPassword(String Hashedpassword, String password ){
+        return BCrypt.checkpw(password, Hashedpassword);
+    }
+
 }
