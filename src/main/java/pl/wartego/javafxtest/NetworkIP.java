706304df5d9f1da9c2c6113c64b7e9b6616522e2
Tomek <wartego@wp.pl>
2023-08-07 18:40:10 +0200

    Deleted some unused dependency

--- src/main/java/pl/wartego/javafxtest/NetworkIP.java
+++ src/main/java/pl/wartego/javafxtest/NetworkIP.java
@@ -7,7 +7,7 @@ import java.net.URL;
 
 public class NetworkIP {
     public String getIP() throws Exception {
-        String urlString = "http://checkip.amazonaws.com/";
+        String urlString = "https://checkip.amazonaws.com/";
         URL url = new URL(urlString);
         try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
             return br.readLine();
