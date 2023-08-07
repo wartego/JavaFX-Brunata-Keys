b9845dbe3af2ce1a252ab975c6c290c2974f5304
Tomek <wartego@wp.pl>
2023-08-07 18:33:25 +0200

    Deleted some unused dependency

--- src/main/java/pl/wartego/javafxtest/NetworkIP.java
+++ src/main/java/pl/wartego/javafxtest/NetworkIP.java
@@ -1,12 +1,8 @@
 package pl.wartego.javafxtest;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class NetworkIP {
@@ -14,11 +10,13 @@ public class NetworkIP {
         String urlString = "http://checkip.amazonaws.com/";
         URL url = new URL(urlString);
         try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-             return br.readLine();
+            return br.readLine();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+    //todo
 }
 
 
